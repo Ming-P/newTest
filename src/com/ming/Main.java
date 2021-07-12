f7fc648edd791e10703e6315d4e20afa39bf43b6
unknown <allinpay@YFZX-Pengmm.allinpay.corp>
2021-07-12 14:10:40 +0800

    Initial commit

---
+++ src/com/ming/Main.java
@@ -0,0 +1,12 @@
+package com.ming;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("TEST");
+
+        int i = 1;
+
+        System.out.println(i);
+
+    }
+}
